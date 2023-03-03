99de8d02ef56e19c0e003772ab0683e2e208b418
dorisavedikian <m>
2023-03-03 14:41:03 -0800

    1st commit - with content

--- _build/jupyter_execute/book/TOC/TableofContent.py
+++ _build/jupyter_execute/book/TOC/TableofContent.py
@@ -43,15 +43,16 @@
 # 
 # 
 # ## MARC Format
-# 
+# ```{sidebar} 
+# > __650__  NALT terms 
+# >
+# > __651__  Geographical terms 
+# >
+# > __653__ Identifiers[^1]
+# ```
 # MARC (MAchine-Readable Cataloging) is used to define three fields that appear in Agricola records; a fourth field for subject category codes is no longer used. While MARC fields may appear on some external platforms, they are largely transparent in the indexing process. NAL systems that manage both human-indexed and machine-indexed articles automatically assign the appropriate field for each term.    
 # 
-# > __650__  NALT terms 
-# 
-# > __651__  Geographical terms 
-# 
-# > __653__ Identifiers (this field is limited to machine-indexed articles and is automatically populated by the system, as needed)
-# 
+# [^1]: This field is limited to machine-indexed articles and is automatically populated by the system, as needed.
 # 
 # ## The NAL Thesaurus (NALT)
 # 
@@ -122,11 +123,10 @@
 #     *	__food processing__, not __processing food__ or __processing of food__
 
 # ```{margin} __Examples of hyphenated terms:__
-# * __1-aminocyclopropane-1-carboxylate deaminase__
-# * __3’ untranslated region__
-# * __blood-brain barrier__
-# * __Fusarium oxysporum f. sp. radicis-lycopersici__
-# * __public-private partnerships__
+# * 1-aminocyclopropane-1-carboxylate deaminase
+# * 3’ untranslated region
+# * blood-brain barrier
+# * public-private partnerships
 # ```
 # *	Avoid punctuation, including hyphens, if possible. Some exceptions include (but are not exclusively) taxonomic names, chemical compounds, scientific terms and accepted terms from other thesauri.
 
